0d3a74d2d0b0fd00e4eae216910730cd59f9cb84
Melanie Voloshynska <71847995+Melania00@users.noreply.github.com>
2023-12-04 20:09:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from kmp import KMP
 
-def main(self):
+
+def main():
+    kmp = KMP()
     haystack = input("Enter the haystack: ")
     needle = input("Enter the needle to search for: ")
 
-    indices = self.search(needle, haystack)
+    indices = kmp.search_all(needle, haystack)
 
     if indices:
-        lps = [idx for idx in range(indices[0], indices[0] + len(needle))]
+        lps = []
+        for index in indices:
+            new_indices = [i for i in range(index, index + len(needle)) if i not in lps]
+            lps.extend(new_indices)
+
         print(f"LPS array containing matched character indices: {lps}")
     else:
         print("The needle was not found in the haystack.")
 
 
 if __name__ == "__main__":
-    kmp = KMP()
-    kmp.main()
+    main()
